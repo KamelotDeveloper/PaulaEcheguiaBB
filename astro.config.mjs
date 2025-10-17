5569dde772c1459b1f4bc6282153831c13775b08
Kamelotdeveloper <giuliano-angel@hotmail.com>
2025-10-17 15:28:50 -0300

    171025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://example.com", // [CHANGEME]
-});
+   // ... otras configuraciones
+      devToolbar: {
+        enabled: false,
+      },
+    });
+
+
